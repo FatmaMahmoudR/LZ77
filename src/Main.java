91b9ccc4e7a2cd49f77c9a15d89daa3004648b7d
FatmaMahmoudR <98098901+FatmaMahmoudR@users.noreply.github.com>
2023-01-04 23:15:46 +0200

    upload main

--- src/Main.java
+++ src/Main.java
@@ -1,161 +1,179 @@
 //Fatma Mahmoud Ramadan 20201134
 //Basmalla Magdy Mohammed 20201045
 
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
 
-
 	public static void main(String[] args) {
-		//////////////
+		// TODO Auto-generated method stub
+
 		
-		System.out.println("Enter 1 to encode string ");
+		
+		String s;
+	    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Hello dear user");
+	    System.out.println("Enter 1 to encode string ");
 	    System.out.println("Enter 2 to decode tags");
-	    Scanner O1 = new Scanner(System.in); 
-	    int op = O1.nextInt();
-	    
-	    
-	    if(op==1) 
-	    {
-	    	String s; 
-			
-			 Scanner O2 = new Scanner(System.in); 
-				s = O2.nextLine();
-		
-				
-			 Map<String, Integer> dict = new HashMap<String, Integer>(); 
-		    
-		    char c1='A', c2='a';
-		    for (int i=65;i<=90;i++){
-		        String tmp="";
-		        tmp+= c1;
-		        c1++;
-		        dict.put(tmp,i);
-		    }
-		    for (int i=97;i<=122;i++){
-		        String tmp="";
-		        tmp+= c2;
-		        c2++;
-		        dict.put(tmp,i);
-		    }
-		    int TagCount = 0;
-		    
-		    int index=128;
-		    for(int i=0,j=1;i<=s.length() && i+j < s.length();){
+	    int op = myObj.nextInt();
+	 
+	  while(op!=1&&op!=2)
+	  {
+		    System.out.println("Invalid operation,Try again");
+		    System.out.println("Enter 1 to encode string ");
+		    System.out.println("Enter 2 to decode tags");
+		    System.out.println("Enter 3 to end the prgram");
+		    op = myObj.nextInt();
+		    if(op==3)break;
+	  }
+	  
+	   if(op==1)
+	   {
+		   int mxpos=-1000000000;
+		   int mxlen=-1000000000;
+	    Scanner Obj = new Scanner(System.in);  // Create a Scanner object
+
+		    s = Obj.nextLine();
+		    ArrayList<Integer> position = new ArrayList<Integer>();
+		    ArrayList<Integer> length = new ArrayList<Integer>();
+		    ArrayList<Character> nextSympol = new ArrayList<Character>();
+		    String sub="";
+		    int start=0;
+	    	boolean found=false;
+	   
+	    	int pos=0;
+	    	int matching=0;
+		    for(int i=0;i<s.length();i++)
+		    {
 		    	
-		        String curr ="";
-		        curr+=s.charAt(i);
-		        String next=curr;
-		      
-	            next+=s.charAt(i+j);
-	           
-		        while(true){   
-		            j++;
-		            
-		            if (dict.containsKey(next) && i+j < s.length()){
-		            	//found
-		                curr=next;
-		                next+=s.charAt(i+j);
+		    	if(found) {
+		    	sub=sub+s.charAt(i);
+		    	found=false;
+		    	matching=0;
+		    	}
+		    	else
+		    	{
+		    		sub="";
+		    		sub=sub+s.charAt(i);
+		    		start=i;
+		    		pos=0;
+		    		matching=0;
+		    	}
+		    	
+		    	for(int j=0;j<start;j++)
+		    	{
+		    		int jj=j,ii=0;
+		    		while(jj<start&&ii<sub.length()&&s.charAt(jj)==sub.charAt(ii))
+		    		{
+		    			ii++;
+		    			jj++;
+		    			matching++;
+		    		}
+		    		if(ii==sub.length())
+		    		{
+		    			found=true;
+		    			pos=j;
+		    		}
+		    		
+		    	}
+		    	if(!found)
+		    	{
+		    		  if(matching==0)
+		                {
+		                    pos=0;
+		                    start=0;
 		                }
-		            else if(i+j == s.length()) { /////
-		            	curr=next;
-		            }
-		            else{ //not found
-		            	
-		                System.out.print(dict.get(curr));
-		                System.out.print(" ");
-		                TagCount++;
-
-		             
-		                dict.put(next,index++);
-		                j=1;
-		                i+=curr.length();
-		                break;
-		           
-		            	
-		            }
-		        }
-		        
-
-
+		    		position.add(start-pos);
+		    		length.add(sub.length()-1);	
+		    		nextSympol.add(s.charAt(i));
+		    	}
+		    	
+		    }
+		    for(int i=0;i<position.size();i++)
+		    {
+		    	System.out.print("< ");
+		    	System.out.print(position.get(i));
+		    	mxpos=Math.max(mxpos,position.get(i));
+		    	System.out.print(" , ");
+		    	System.out.print(length.get(i));
+		    	mxlen=Math.max(mxlen,length.get(i));
+		    	System.out.print(" , ");
+		    	System.out.print(nextSympol.get(i));
+		    	System.out.println(" >");
+		    	
+		    }
+		    int cntpos=0,cntlen=0;
+		    while(mxpos>0)
+		    {
+		    	mxpos/=2;
+		    	cntpos++;
+		    }
+		    while(mxlen>0)
+		    {
+		    	mxlen/=2;
+		    	cntlen++;
 		    }
 		    
-		    System.out.print("\n");
-		    System.out.print("Original Size : "+ s.length()*8+" Bits\n");
-		    System.out.print("Compressed Size : "+ TagCount*8+" Bits\n");
 
-			//////////
+	    	System.out.println("Size of original string = "+(8*s.length()));
+	    	System.out.println("Size of compressed string = "+(position.size()*(8+cntpos+cntlen)));
 	    	
-	    }
-	    else 
-	    {
-	    	System.out.println("Enter your number of Tags : ");
-			 Scanner INPUT = new Scanner(System.in);
-			 int n=INPUT.nextInt();
 
-			 System.out.println("Enter your tags : ");
-			int [] tags=new int[n];
-			for(int i=0;i<n;i++) {
-				tags[i]=INPUT.nextInt();
-				
-			}
-			String original_data="";
-			int c=128;
-			Map <Integer,String>m=new HashMap<Integer,String>();
-			for(int i=65;i<=127;i++){
-				   
-			    m.put(i,"" +(char)i);
-			}
-			int tags_num=tags.length;
-			 for(int i=0;i<tags_num;i++){
-		         if(i!=0){
-		       int x=tags[i];
-		       if(m.get(x)!=null){
-			        String v="";
-			        v=m.get(tags[i-1]);
-			        String vv=m.get(x);
-			      
-			        v+=vv.substring(0,1);
-			        m.put(c,v);
-			        original_data+=m.get(x);
-			        c++;
-		       }
-		       
-		       else{
-		            String cur=m.get(tags[i-1]);
-		            String v="";
-		            v+=cur+cur.substring(0,1);
-		            m.put(c, v);
-		            original_data+=v;
-		            c++;
-		       }
-		       }
-	         else{
-	        	 original_data+=m.get(tags[i]);
-	            }    
-		
-			 }
+
+		   
+	    	
+	   }
+	   else if(op==2)
+       {
+		   
+
+		   String ss = "<0,0,A><0,0,B><2,1,A><3,2,B><5,3,B><2,2,B><5,5,B><1,1,A> ";
+			
+		   Scanner O = new Scanner(System.in);  // Create a Scanner object
+			 ss = O.nextLine();
+			String Compressed = "";
+
+			 for (int i = 0; i < ss.length(); i++)
+			 {
+			        if ( ss.charAt(i) == '<' || ss.charAt(i) == '>' || ss.charAt(i) == ',')
+			            continue;
+			        else
+			        	Compressed += ss.charAt(i);
+			        }
 			 
-				System.out.println("The original data is");
-				System.out.println(original_data);
-				System.out.println("..........................");
-				System.out.print("Original Size : ");
-				System.out.println(original_data.length()*8);
-				System.out.println("..........................");
-			 }
-	    
-	    }
-	    	
-	    	
+			//00A00B21A32B53B22B55B11A
+			 
+			 Vector v = new Vector<Character>();
+
+			    for (int i = 0; i < Compressed.length() - 1; i += 3)
+			    {
+			        int x = Compressed.charAt(i) - 48;
+			        int y = Compressed.charAt(i+1) - 48;
+
+			        int start = v.size() - x;
+			        
+			        for (int j = start; j < start + y; j++) 
+			        {
+			            v.add(v.get(j));
+
+			           
+			        }
+			        v.add(Compressed.charAt(i+2));
+
+			    }
+
+			    for (int i = 0; i < v.size(); i++) 
+			    {
+			    	System.out.print(v.get(i));
+			    }
+
+       }
+	    //ABAABABAABBBBBBBBBBBBA
+	   //<0,0,A><0,0,B><2,1,A><3,2,B><5,3,B><2,2,B><5,5,B><1,1,A>
+
 	
+	}
+
 }
-
-
-//ABAABABBAABAABAAAABABBBBBBBB
-//14
-//65 66 65 128 128 129 131 134 130 129 66 138 139 138
-
